8826c35fe666220c6386ea8e7a5fb160ddae2b1a
Ejicha <86265693+Ejicha@users.noreply.github.com>
2022-04-07 18:00:20 -0500

    Update restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -54,7 +54,7 @@ def get_dealer_by_id_from_cf(url, id):
     
         dealer_doc = dealers["docs"][0]
         dealer_obj = CarDealer(address=dealer_doc["address"], city=dealer_doc["city"],
-                                id=dealer_doc["id"], lat=dealer_doc["lat"], long=dealer_doc["long"],
+                                full_name=dealer_doc["full_name"], id=dealer_doc["id"], lat=dealer_doc["lat"], long=dealer_doc["long"],
                                 
                                 st=dealer_doc["st"], zip=dealer_doc["zip"])
     return dealer_obj
