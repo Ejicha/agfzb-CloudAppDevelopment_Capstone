6dc2ab6e40df49296575178ec72afd0740403c68
Ejicha <86265693+Ejicha@users.noreply.github.com>
2022-04-13 20:10:10 -0500

    Update admin.py

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -9,7 +9,7 @@ class CarModelInline(admin.StackedInline):
 
 # CarModelAdmin class
 class CarModelAdmin(admin.ModelAdmin):
-    fields=["car_make", "dealer_id", "name", "type", "year"]
+    fields=["car_make", "name", "id", "type", "year"]
 
 # CarMakeAdmin class with CarModelInline
 class CarMakeAdmin(admin.ModelAdmin):
